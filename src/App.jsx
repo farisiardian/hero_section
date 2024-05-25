fbf0f96ab5a4c3a133277736ede03eb013e5f63d
farisiardian <101421278+farisiardian@users.noreply.github.com>
2024-05-25 09:21:27 +0700

    Tiket 001: Background [App]

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,60 @@
-import { useState } from "react";
 import "@src/App.css";
-import Hero from "./HeroSection";
+import ilustrasi from "./assets/images/illustration-mockups.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faInstagram,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { faX } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <Hero />
+      <header className="wrapper">
+        <img className="img" aria-hidden="true" src={ilustrasi} alt="" />
+        <div className="hero">
+          <h1 className="title">Build The Community Your Fans Will Love</h1>
+          <p className="txt">
+            Huddle re-imagines the way we build communities. You have a voice,
+            but so does your audience. Create connections with your users as you
+            engage in genuine discussion.
+          </p>
+          <button className="btn">Register</button>
+        </div>
+        <ul className="social">
+          <li>
+            <a
+              target="_blank"
+              href="https://facebook.com"
+              aria-label="Visit our facebook page"
+            >
+              {/* You can add your Facebook icon or SVG here */}
+              <FontAwesomeIcon icon={faFacebookF} />
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              href="https://twitter.com"
+              aria-label="Visit our twitter page"
+            >
+              {/* You can add your Twitter icon or SVG here */}
+              <FontAwesomeIcon icon={faTwitter} />
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              href="https://instagram.com"
+              aria-label="Visit our instagram page"
+            >
+              {/* You can add your Instagram icon or SVG here */}
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+          </li>
+        </ul>
+      </header>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,83 @@ export default {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#fff",
+        violet: "hsl(257, 40%, 49%)",
+        softMagenta: "hsl(300, 69%, 71%)",
+      },
+      fontFamily: {
+        body: ["Open Sans", "sans-serif"],
+        heading: ["Poppins", "sans-serif"],
+      },
+      screens: {
+        sm: "600px",
+        md: "992px",
+      },
+      boxShadow: {
+        custom: "0 6px 3px rgba(0, 0, 0, 0.15)",
+      },
+      borderRadius: {
+        custom: "40px",
+        full: "9999px",
+      },
+      maxWidth: {
+        custom: "590px",
+        full: "100%",
+      },
+      minWidth: {
+        custom: "400px",
+      },
+      padding: {
+        custom: "65px 80px",
+        sm: "24px",
+      },
+      margin: {
+        custom: "126px 120px",
+        btn: "50px 130px",
+        social: "24px 0",
+      },
+      fontSize: {
+        customTitle: "3rem",
+        customTxt: "1.9774rem",
+        btn: "1.5rem",
+        mdTitle: "2.5rem",
+        mdTxt: "18px",
+      },
+      gridTemplateColumns: {
+        custom: "minmax(0, 696px) minmax(0, 508px)",
+      },
+      scale: {
+        custom: "0.75",
+        social: "0.8",
+      },
+      translate: {
+        "-fullX": "-48px",
+      },
+      gridColumn: {
+        full: "1 / -1",
+        social: "3 / -3",
+      },
+      gap: {
+        social: "10px",
+        sm: "17px",
+      },
+      borderWidth: {
+        custom: "1px",
+      },
+      height: {
+        btn: "80px",
+        sm: "50px",
+        md: "55px",
+      },
+      width: {
+        customBtn: "400px",
+        mdBtn: "200px",
+        sm: "50px",
+        md: "38px",
+      },
+    },
   },
   plugins: [],
 };
